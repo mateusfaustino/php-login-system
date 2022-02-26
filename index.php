dba69cfd69aae8b16f8347967cd48c41e5a101e8
Mateus Faustino <mateusfaustino2017@gmail.com>
2022-02-26 17:08:51 -0300

    favicon

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php echo "all right";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
+    <title>Login System</title>
+</head>
+<body>
+    <h1>Login</h1>
+</body>
+</html>
