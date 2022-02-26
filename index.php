0b6bf69a5b97e05d15a2da27bc9f33a88562031b
Mateus Faustino <mateusfaustino2017@gmail.com>
2022-02-26 19:24:08 -0300

    html index structure

--- index.php
+++ index.php
@@ -8,6 +8,102 @@
     <title>Login System</title>
 </head>
 <body>
-    <h1>Login</h1>
+    <main>
+        <div class="brand">
+            <div class="brand__logo">
+                <img class="brand__logo__img" src="assets/logo.svg" alt="">
+            </div>
+            <div class="brand__text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam consequuntur, modi sequi, asperiores expedita, rerum eligendi quisquam consectetur debitis beatae corrupti amet recusandae 
+            </div>
+        </div>
+        <div class="auth">
+            <form class="auth__form" action="">
+                <div class="auth__input-wrapper">
+                    <input type="text">
+                </div>
+
+                <div class="auth__input-wrapper">
+                    <input type="text">
+                </div>
+                
+                <div class="auth__input-wrapper">
+                    <button>
+                        Entrar
+                    </button>
+                </div>
+            </form>
+            <div class="auth__forgot-password-wrapper">
+                <a href="" class="auth__forgot-password__link">
+                    Esqueceu a senha
+                </a>
+            </div>
+            <div class="auth__line"></div>
+            <div class="auth__create-account-wrapper">
+                <button>
+                    Criar nova conta
+                </button>
+            </div> 
+        </div>
+        <div class="signup">
+            <div class="signup__modal">
+                <div class="signup__modal__header">
+                    <div class="signup__modal__header__title">
+                        Cadastre-se
+                    </div>
+                    <div class="signup__modal__header__description">
+                        É rápido e fácil.
+                    </div>
+                </div>
+                <div class="signup__modal__body">
+                    <form action="" class="signup__form">
+                        <div class="auth__input-wrapper">
+                            <input type="text">
+                        </div>
+                        <div class="input-group">
+                            <div class="input-group__title">
+                                data de nascimento
+                            </div>
+                            <div class="signup__input-wrapper">
+                                <input type="text">
+                            </div>
+                            <div class="signup__input-wrapper">
+                                <input type="text">
+                            </div>
+                            <div class="signup__input-wrapper">
+                                <input type="text">
+                            </div>
+                        </div>
+                        <div class="input-group">
+                            <div class="input-group__title">
+                                Gênero
+                            </div>
+                            <div class="signup__input-wrapper">
+                                <input type="text">
+                            </div>
+                            <div class="signup__input-wrapper">
+                                <input type="text">
+                            </div>
+                            <div class="signup__input-wrapper">
+                                <input type="text">
+                            </div>
+                        </div>
+                        <div class="signup__terms">
+                            Ao clicar em Cadastre-se, você concorda com nossos Termos, Política de Dados e Política de Cookies. Você poderá receber notificações por SMS e cancelar isso quando quiser.
+                        </div>
+                        <div class="auth__create-account-wrapper">
+                            <button>
+                                Cadastre-se
+                            </button>
+                        </div> 
+                    </form>
+                    <div class="signup__modal__close">
+                        x
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
 </body>
 </html>
